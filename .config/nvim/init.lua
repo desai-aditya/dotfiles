573ce6d346196e9f98e96c1a125ac6b585abc483
Aditya Desai <adityadesai@Adityas-MacBook-Air.local>
2024-09-02 11:44:45 -0700

    Add rust-analyzer lsp

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -110,8 +110,6 @@ vim.opt.mouse = 'a'
 -- Don't show the mode, since it's already in the status line
 vim.opt.showmode = false
 
--- Sync clipboard between OS and Neovim.
---  Schedule the setting after `UiEnter` because it can increase startup-time.
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
 vim.schedule(function()
@@ -608,7 +606,7 @@ require('lazy').setup({
         -- clangd = {},
         -- gopls = {},
         -- pyright = {},
-        -- rust_analyzer = {},
+        rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
